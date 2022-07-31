19483d62940bd2a5247e2d0fc5459750872b9856
Joseph chinedu <josephchinedu34@gmail.com>
2022-08-01 00:23:44 +0100

    set web port in heroku env

--- main.go
+++ main.go
@@ -5,13 +5,25 @@ import (
 	"go-postgres/router"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
 	r := router.Router()
 	// fs := http.FileServer(http.Dir("build"))
 	// http.Handle("/", fs)
 	fmt.Println("Starting server on the port 8080...")
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	port := ":" + os.Getenv("PORT")
+
+	log.Fatal(http.ListenAndServe(port, r))
 }
